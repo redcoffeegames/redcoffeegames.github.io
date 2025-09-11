dfd44a42bbdd9c130f8b204996024e8dd168eb92
Red Coffee Games <studio.coffeered@gmail.com>
2025-09-11 15:49:51 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 window.addEventListener('scroll', function() {
-  const bg = document.querySelector('.full-screen-bg');
   const header = document.querySelector('.animated-header');
   const scrollPosition = window.scrollY;
 
   if (scrollPosition > 50) {
-    bg.classList.add('hidden');
     header.classList.add('active');
   } else {
-    bg.classList.remove('hidden');
     header.classList.remove('active');
   }
 });
